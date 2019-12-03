074f5643f530c5660008dcd3af39a897fbf1a685
axptwig <raghuraman@outlook.com>
2019-12-03 10:24:00 -0800

    please work

---
+++ src/graphql/mutations.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createVodAsset = `mutation CreateVodAsset($input: CreateVodAssetInput!) {
+  createVodAsset(input: $input) {
+    id
+    title
+    description
+    video {
+      id
+    }
+  }
+}
+`;
+export const updateVodAsset = `mutation UpdateVodAsset($input: UpdateVodAssetInput!) {
+  updateVodAsset(input: $input) {
+    id
+    title
+    description
+    video {
+      id
+    }
+  }
+}
+`;
+export const deleteVodAsset = `mutation DeleteVodAsset($input: DeleteVodAssetInput!) {
+  deleteVodAsset(input: $input) {
+    id
+    title
+    description
+    video {
+      id
+    }
+  }
+}
+`;
+export const createVideoObject = `mutation CreateVideoObject($input: CreateVideoObjectInput!) {
+  createVideoObject(input: $input) {
+    id
+  }
+}
+`;
+export const updateVideoObject = `mutation UpdateVideoObject($input: UpdateVideoObjectInput!) {
+  updateVideoObject(input: $input) {
+    id
+  }
+}
+`;
+export const deleteVideoObject = `mutation DeleteVideoObject($input: DeleteVideoObjectInput!) {
+  deleteVideoObject(input: $input) {
+    id
+  }
+}
+`;
